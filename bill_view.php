530120d704a25b23eb28244e1d4790daf7d5334f
ARBhuiyanSP <arbhuiyan.pits@gmail.com>
2024-02-05 13:59:08 +0600

    first commit

--- bill_view.php
+++ bill_view.php
@@ -57,27 +57,23 @@ $row = mysqli_fetch_array($result);
 							</table>
 						</div>
 					</div>
-					<center><h5>INVOICE</h5></center>
+					<center><h5>Money Receipt</h5></center>
 					<div class="row">
 						<div class="col-sm-12">
 							<table class="table table-bordered" id="material_receive_list"> 
 								<tr>
-									<td style="width:50%">
-										<b>From</b></br>
-										<b>E Engineering Ltd</b></br>
-										<span>Khawaja Tower[7th Floor]</br>95 Bir Uttam A.K Khandokar Road, Mohakhali C/A</br>Dhaka-1212, Bangladesh</span>
-									</td>
 									<td style="width:50%">
 										<b>To [Client Info]</b></br>
 										<b><?php echo getNameByIdAndTable('clients',$rowDetails["client_name"]); ?></b></br>
 										<?php echo getProjectNameById($rowDetails["project_name"]); ?></br>
 										<?php echo getProjectAddressById($rowDetails["project_name"]); ?>
 									</td>
+									<td></td>
 								</tr>
 							</table>
 						</div>
 					</div>
-					<center><h5>Rented Equipment List</h5></center>
+					<center><h5></h5></center>
 					<div id="employee_table">  
 						<table class="table table-bordered table-striped">  
 						   <tr>  
@@ -86,14 +82,12 @@ $row = mysqli_fetch_array($result);
 								<th style="width:10%;">Amount</th>   
 						   </tr>  
 						   <?php
-							$totalamount = 0;
 							$id = $_GET['no'];
-							$queryData = "SELECT * FROM rent_bill where `id`='$id' ORDER BY id DESC";  
+							$queryData = "SELECT * FROM `rent_bill` WHERE `id`='$id' ORDER BY id DESC";  
 							$resultData = mysqli_query($conn, $queryData);
-							$rowData = mysqli_fetch_array($resultData);
-						   while($rowData = mysqli_fetch_array($resultData))  
-						   {  
-								$totalamount += $row["amount"];
+							//$rowData = mysqli_fetch_array($resultData);
+							   while($rowData = mysqli_fetch_array($resultData))  
+							   {  
 						   ?>  
 						   <tr>  
 								<td><?php echo $row["bill_no"]; ?></td>  
@@ -103,21 +97,13 @@ $row = mysqli_fetch_array($result);
 						   <?php  
 						   }  
 						   ?> 
-							<tr>  
-								<td colspan="3" style="text-align:right"><b>Sub Total:</b></td>    
-								<td><?php echo $totalamount; ?></td>    
-						   </tr>
 						   <tr>  
-								<td colspan="3" style="text-align:right"><b>Discount:</b></td>    
-								<td><?php echo $rowDetails["discount"]; ?></td>    
-						   </tr>
-						   <tr>  
-								<td colspan="3" style="text-align:right"><b>Grand Total:</b></td>    
-								<td><?php echo $grandtotal = $totalamount - $rowDetails["discount"]; ?></td>    
+								<td colspan="2" style="text-align:right"><b> Total:</b></td>    
+								<td><?php echo $row["amount"]; ?></td>    
 						   </tr>
 						   <tr>
-								<td colspan="4" class="grand_total" style="text-align:left;">
-									<b>Amount In Words :<span style="font-style: italic;text-decoration:underline;"><?php echo convertNumberToWords($grandtotal);?></span> Only</b>
+								<td colspan="2" class="grand_total" style="text-align:left;">
+									<b>Amount In Words :<span style="font-style: italic;text-decoration:underline;"><?php echo convertNumberToWords($row["amount"]);?></span> Only</b>
 								</td>
 							</tr>
 					  </table>  
